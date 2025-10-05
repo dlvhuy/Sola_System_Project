dac37f2af43ee5ef3c073ce9e6d0840a3f3f1822
hellodlvhuy <vanhuy098420@gmail.com>
2025-10-05 22:09:46 +0700

    feat (UI) (admin): done form addStudentInfo Modal

--- Frontend_Sola/src/app/(home)/admin/page.tsx
+++ Frontend_Sola/src/app/(home)/admin/page.tsx
@@ -8,19 +8,27 @@ import { useState } from "react";
 export default function AdminPage() {
 
     const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
-    
-         const handleSetOpenModal = (data: boolean) => {
-                setIsOpenModal(data);
-        };
-    
-        
+
+    const handleSetOpenModal = (data: boolean) => {
+        setIsOpenModal(data);
+    };
+
+
     return (
         <Card className="my-4 p-4 border-0 shadow-none ">
+            <div className="flex gap-2 ">
+                <Button variant="link"  className="w-[100px] cursor-pointer font-bold">
+                    Giáo viên
+                </Button>
+                <Button variant="link" className="w-[100px] cursor-pointer font-bold">
+                    Học sinh
+                </Button>
+            </div>
             <div className="grid lg:grid-cols-3 gap-4  md:*:grid-cols-1 sm:grid-cols-1">
                 <div className=" grid lg:col-span-2 p-4 rounded-lg ">
                     <div className="flex justify-between">
                         <div className="max-w-[80%]" >
-                            <h1 className="text-2xl font-bold mb-4">Trang Quản Trị Viên</h1>
+                            <h1 className="text-2xl font-bold mb-4">Danh sách học viên</h1>
                         </div>
                         <div>
                             <Button className="cursor-pointer" onClick={() => setIsOpenModal(true)}>
@@ -30,26 +38,22 @@ export default function AdminPage() {
                     </div>
                 </div>
                 <div className="border-2 p-4 rounded-lg">
-
                     <h1 className="text-2xl font-bold mb-4">Trang Quản Trị Viên</h1>
                     <p>Chào mừng bạn đến với trang quản trị viên. Tại đây bạn có thể quản lý hệ thống.</p>
-
                 </div>
                 <div className="border-2 p-4 rounded-lg">
-
                     <h1 className="text-2xl font-bold mb-4">Trang Quản Trị Viên</h1>
                     <p>Chào mừng bạn đến với trang quản trị viên. Tại đây bạn có thể quản lý hệ thống.</p>
-
                 </div>
             </div>
 
-             {
-                    isOpenModal && <div className="bg-gray-100 min-h-screen p-4">
-                        <Modal sendOpenModal={handleSetOpenModal}>
-                            <FormAddInfoStudent></FormAddInfoStudent>
-                        </Modal>
-                    </div>
-                }
+            {
+                isOpenModal && 
+                    <Modal sendOpenModal={handleSetOpenModal} title="Thêm thông tin học sinh">
+                        <FormAddInfoStudent></FormAddInfoStudent>
+                    </Modal>
+                
+            }
         </Card>
     )
 }

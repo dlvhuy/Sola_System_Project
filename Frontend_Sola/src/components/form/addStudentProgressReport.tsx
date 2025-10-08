f5698cd0333d50169627c627da672f16e8aa18f9
HUY DO <vanhuy098420@gmail.com>
2025-10-08 16:00:29 +0700

    feat (UI): Done validation for form addStudentProgressReport.

--- Frontend_Sola/src/components/form/addStudentProgressReport.tsx
+++ Frontend_Sola/src/components/form/addStudentProgressReport.tsx
@@ -4,49 +4,126 @@ import DatePicker from "../datePicker"
 import { Textarea } from "../ui/textarea"
 import { Slider } from "../ui/slider"
 import { Button } from "../ui/button"
+import { useState } from "react"
+
+
+
+type CommentField = {
+    Class: number,
+    comment: string
+}
+type CommentFieldError = {
+    Class?: number,
+    comment: string
+}
+
+type InfoStudentProgressReport = {
+    lesson: string,
+    lessonDate: string,
+    thinkingSkill: CommentField,
+    attitude: CommentField,
+    theorySkill: CommentField,
+    practiceSkill: CommentField
+}
+
+const initialFormData: InfoStudentProgressReport = {
+    lesson: "",
+    lessonDate: "",
+    thinkingSkill: {
+        Class: 1,
+        comment: ""
+    },
+    attitude: {
+        Class: 1,
+        comment: ""
+    },
+    theorySkill: {
+        Class: 1,
+        comment: ""
+    },
+    practiceSkill: {
+        Class: 1,
+        comment: ""
+    },
+}
+
+type InfoStudentProgressReportError = {
+    lesson?: string,
+    lessonDate?: string,
+    thinkingSkill?: CommentFieldError,
+    attitude?: CommentFieldError,
+    theorySkill?: CommentFieldError,
+    practiceSkill?: CommentFieldError
+}
 
 
 export default function FormAddStudentProgressReport() {
-
+    const [addInfoStudentProgressReportData, setAddInfoStudentProgressReportData] = useState<InfoStudentProgressReport>(initialFormData)
+    const [addInfoStudentProgressReportDataError, setAddInfoStudentProgressReportDataError] = useState<InfoStudentProgressReportError>({})
     function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault()
-        // const { name, birthDay, gender, phoneNumber, email, address } = addInfoTeacherData
-        // let infoTeacherError: InfoTeacherError = {}
+        const { lesson, lessonDate, thinkingSkill, attitude, theorySkill, practiceSkill } = addInfoStudentProgressReportData
+        let addInfoStudentProgressReportDataError: InfoStudentProgressReportError = {}
 
-        // const nameTeacherRegex = /^[A-Za-zÀ-Ỹà-ỹĐđ\s]+$/
-        // if (!nameTeacherRegex.test(name)) {
-        //     infoTeacherError.name = "Tên giáo viên không hợp lệ"
-        // }
+        const lessonRegex = /^[A-Za-zÀ-Ỹà-ỹĐđ0-9\s]+$/
+        if (!lessonRegex.test(lesson)) {
+            addInfoStudentProgressReportDataError.lesson = "Tên buổi học không hợp lệ"
+        }
 
-        // const genderOptions = ["Nam", "Nữ"]
+        if (new Date(lessonDate) > new Date()) {
+            addInfoStudentProgressReportDataError.lessonDate = "Ngày học không hợp lệ"
+        }
 
-        // if (!genderOptions.includes(gender)) {
-        //     infoTeacherError.gender = "Giới tính phải là Nam hoặc Nữ"
-        // }
+        if (!thinkingSkill.comment || thinkingSkill.comment === "") {
+            addInfoStudentProgressReportDataError.thinkingSkill = {
+                ...addInfoStudentProgressReportDataError.thinkingSkill,
+                comment: "Nhận xét tư duy không được để trống"
+            };
+        }
 
-        // if (new Date(birthDay) > new Date()) {
-        //     infoTeacherError.birthDay = "Ngày sinh không hợp lệ"
-        // }
 
-        // const phoneNumberRegex = /^[0-9]{10}$/
-        // if (!phoneNumberRegex.test(phoneNumber)) {
-        //     infoTeacherError.phoneNumber = "Số điện thoại phải là số và có 10 ký tự"
-        // }
+        if (!attitude.comment || attitude.comment === "") {
+            addInfoStudentProgressReportDataError.attitude = {
+                ...addInfoStudentProgressReportDataError.attitude,
+                comment: "Nhận xét thái độ không được để trống"
+            };
+        }
 
-        // const detailAddressRegex = /^[A-Za-zÀ-Ỹà-ỹĐđ0-9\s]+$/
-        // if (!detailAddressRegex.test(address.detailAddress)) {
-        //     infoTeacherError.address = {
-        //         ...infoTeacherError.address,
-        //         detailAddress: "Dịa chỉ không hợp lệ"
-        //     }
-        // }
-        // setAddInfoTeacherDataError(infoTeacherError)
+        if (!theorySkill.comment || theorySkill.comment === "") {
+            addInfoStudentProgressReportDataError.theorySkill = {
+                ...addInfoStudentProgressReportDataError.theorySkill,
+                comment: "Nhận xét lý thuyết không được để trống"
+            };
+        }
 
-        // if (Object.keys(infoTeacherError).length === 0) {
-        //     console.log("Form submitted:", { addInfoTeacherData })
-        // }
+        if (!practiceSkill.comment || practiceSkill.comment === "") {
+            addInfoStudentProgressReportDataError.practiceSkill = {
+                ...addInfoStudentProgressReportDataError.practiceSkill,
+                comment: "Nhận xét thực hành không được để trống"
+            };
+        }
+
+        setAddInfoStudentProgressReportDataError(addInfoStudentProgressReportDataError)
+
+        if (Object.keys(addInfoStudentProgressReportDataError).length === 0) {
+            console.log("Form submitted:", { addInfoStudentProgressReportData })
+        }
 
     }
+
+    const updateNestedField = (
+        parentKey: keyof typeof addInfoStudentProgressReportData,
+        fieldKey: string,
+        newValue: any
+    ) => {
+        setAddInfoStudentProgressReportData((prev) => ({
+            ...prev,
+            [parentKey]: {
+                ...(typeof prev[parentKey] === "object" && prev[parentKey] !== null ? prev[parentKey] : {}),
+                [fieldKey]: newValue,
+            },
+        }));
+    };
 
     return (
         <form onSubmit={handleSubmit} className="w-[1000px]">
@@ -58,19 +135,21 @@ export default function FormAddStudentProgressReport() {
                             className="h-10 w-60"
                             id="nameStudent"
                             type="text"
+                            value={addInfoStudentProgressReportData.lesson}
+                            onChange={e => setAddInfoStudentProgressReportData({ ...addInfoStudentProgressReportData, lesson: e.target.value })}
                             placeholder="Ví dụ: Lập trình cơ bản"
                             required
                         />
-                        {/* {addInfoStudentDataError.student?.nameStudent && <p className="px-2 text-sm text-red-500">{addInfoStudentDataError.student?.nameStudent}</p>} */}
+                        {addInfoStudentProgressReportDataError.lesson && <p className="px-2 text-sm text-red-500">{addInfoStudentProgressReportDataError.lesson}</p>}
                     </div>
                     <div className="grid gap-2">
                         <Label htmlFor="birthDay">Ngày học</Label>
                         <DatePicker
                             className="w-40"
-                        // value={addInfoTeacherData.birthDay ? new Date(addInfoTeacherData.birthDay) : undefined}
-                        // onChange={(date) => setAddInfoTeacherData({ ...addInfoTeacherData, birthDay: date ? date.toISOString() : "" })}
+                            value={addInfoStudentProgressReportData.lessonDate ? new Date(addInfoStudentProgressReportData.lessonDate) : undefined}
+                            onChange={(date) => setAddInfoStudentProgressReportData({ ...addInfoStudentProgressReportData, lessonDate: date ? date.toISOString() : "" })}
                         />
-                        {/* {addInfoTeacherDataError.birthDay && <p className="px-2 text-sm text-red-500">{addInfoTeacherDataError.birthDay}</p>} */}
+                        {addInfoStudentProgressReportDataError.lessonDate && <p className="px-2 text-sm text-red-500">{addInfoStudentProgressReportDataError.lessonDate}</p>}
                     </div>
                 </div>
                 <div className="grid grid-cols-2 grid-rows-2 gap-4">
@@ -80,12 +159,13 @@ export default function FormAddStudentProgressReport() {
                         <div className="flex justify-between items-center gap-2">
                             <div className="w-[80%] px-2 my-2">
                                 <Slider
-                                    id="attitude"
+                                    id="thinkingSkill"
                                     min={1}
                                     max={5}
                                     step={1}
-                                    // value={[reviewData.attitude_rating]}
-                                    // onValueChange={(value) => setReviewData({ ...reviewData, attitude_rating: value[0] })}
+                                    value={[addInfoStudentProgressReportData.thinkingSkill.Class]}
+                                    onValueChange={(value) => updateNestedField("thinkingSkill", "Class", value[0])}
+
                                     className="w-full "
                                 />
                                 <div className="flex mt-2 justify-between text-xs text-gray-500">
@@ -99,8 +179,13 @@ export default function FormAddStudentProgressReport() {
                         </div>
                         <Textarea
                             placeholder="Nhập nhận xét"
+                            value={addInfoStudentProgressReportData.thinkingSkill.comment}
+                            onChange={(e) => updateNestedField("thinkingSkill", "comment", e.target.value)}
                             className=""
                         />
+                        <div className="min-h-[20px]">
+                            {addInfoStudentProgressReportDataError.thinkingSkill?.comment && <p className="px-2 text-sm text-red-500">{addInfoStudentProgressReportDataError.thinkingSkill?.comment}</p>}
+                        </div>
                     </div>
                     <div className="grid gap-2">
                         <Label className="" htmlFor="comment">Thái độ</Label>
@@ -111,8 +196,8 @@ export default function FormAddStudentProgressReport() {
                                     min={1}
                                     max={5}
                                     step={1}
-                                    // value={[reviewData.attitude_rating]}
-                                    // onValueChange={(value) => setReviewData({ ...reviewData, attitude_rating: value[0] })}
+                                    value={[addInfoStudentProgressReportData.attitude.Class]}
+                                    onValueChange={(value) => updateNestedField("attitude", "Class", value[0])}
                                     className="w-full"
                                 />
                                 <div className="flex mt-2 justify-between text-xs text-gray-500">
@@ -126,20 +211,26 @@ export default function FormAddStudentProgressReport() {
                         </div>
                         <Textarea
                             placeholder="Nhập nhận xét"
+                            value={addInfoStudentProgressReportData.attitude.comment}
+                            onChange={(e) => updateNestedField("attitude", "comment", e.target.value)}
                             className=""
                         />
+                        <div className="min-h-[20px]">
+                            {addInfoStudentProgressReportDataError.attitude?.comment && <p className="px-2 text-sm text-red-500">{addInfoStudentProgressReportDataError.attitude?.comment}</p>}
+                        </div>
+
                     </div>
                     <div className="grid gap-2">
                         <Label className="" htmlFor="comment">Lý thuyết</Label>
                         <div className="flex justify-between items-center gap-2">
                             <div className="w-[80%] px-2 my-2">
                                 <Slider
-                                    id="attitude"
+                                    id="theorySkill"
                                     min={1}
                                     max={5}
                                     step={1}
-                                    // value={[reviewData.attitude_rating]}
-                                    // onValueChange={(value) => setReviewData({ ...reviewData, attitude_rating: value[0] })}
+                                    value={[addInfoStudentProgressReportData.theorySkill.Class]}
+                                    onValueChange={(value) => updateNestedField("theorySkill", "Class", value[0])}
                                     className="w-full my-2"
                                 />
                                 <div className="flex mt-2 justify-between text-xs text-gray-500">
@@ -152,21 +243,26 @@ export default function FormAddStudentProgressReport() {
                             </div>
                         </div>
                         <Textarea
+                            value={addInfoStudentProgressReportData.theorySkill.comment}
+                            onChange={(e) => updateNestedField("theorySkill", "comment", e.target.value)}
                             placeholder="Nhập nhận xét"
                             className=""
                         />
+                        <div className="min-h-[20px]">
+                            {addInfoStudentProgressReportDataError.theorySkill?.comment && <p className="px-2 text-sm text-red-500">{addInfoStudentProgressReportDataError.theorySkill?.comment}</p>}
+                        </div>
                     </div>
                     <div className="grid gap-2">
                         <Label className="" htmlFor="comment">Thực hành</Label>
                         <div className="flex justify-between items-center gap-2">
                             <div className="w-[80%] px-2 my-2">
                                 <Slider
-                                    id="attitude"
+                                    id="practiceSkill"
                                     min={1}
                                     max={5}
                                     step={1}
-                                    // value={[reviewData.attitude_rating]}
-                                    // onValueChange={(value) => setReviewData({ ...reviewData, attitude_rating: value[0] })}
+                                    value={[addInfoStudentProgressReportData.practiceSkill.Class]}
+                                    onValueChange={(value) => updateNestedField("theorySkill", "Class", value[0])}
                                     className="w-full my-2"
                                 />
                                 <div className="flex mt-2 justify-between text-xs text-gray-500">
@@ -181,14 +277,23 @@ export default function FormAddStudentProgressReport() {
                         <Textarea
                             rows={3}
                             maxLength={250}
+                            value={addInfoStudentProgressReportData.practiceSkill.comment}
+                            onChange={(e) => updateNestedField("practiceSkill", "comment", e.target.value)}
                             placeholder="Nhập nhận xét"
-                            className="resize-none max-h-[80px] overflow-x-scroll scro" 
+                            className="resize-none max-h-[80px] overflow-x-scroll scro"
                         />
+                        <div className="min-h-[20px]">
+                            {addInfoStudentProgressReportDataError.practiceSkill?.comment && <p className="px-2 text-sm text-red-500">{addInfoStudentProgressReportDataError.practiceSkill?.comment}</p>}
+                        </div>
+
                     </div>
                 </div>
             </div>
             <div className="flex justify-center mt-5">
-                <Button className="cursor-pointer"> Lưu báo báo học tập </Button>
+                <Button
+                    className="cursor-pointer"
+                    type="submit"
+                > Lưu báo báo học tập </Button>
             </div>
         </form>
     )
